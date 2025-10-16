33c1e1fa3a5cc7cf604cda5db219195b97e5a872
steve-232 <steve232np@gmail.com>
2025-10-16 07:50:05 +0200

    add BaseButton.vue

---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button :type="type" :disabled="disabled || loading">
+    <span v-if="loading">Loading...</span><slot v-else></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: "button" | "reset" | "submit";
+  disabled?: boolean;
+  loading?: boolean;
+}
+
+const {
+  type = "button",
+  disabled = false,
+  loading = false,
+} = defineProps<Props>();
+</script>
