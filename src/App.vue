55272020e412b96b2dbe89d3a6b20a7e2a333bc7
steve-232 <steve232np@gmail.com>
2025-10-17 07:50:56 +0200

    add ColumnHorseList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="page">
     <TheHeader />
+    <ColumnHorseList />
   </div>
 </template>
 
 <script setup lang="ts">
 import TheHeader from "@/components/TheHeader.vue";
+import ColumnHorseList from "./components/ColumnHorseList.vue";
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.page-header {
+  margin-bottom: 30px;
+}
+</style>
---
+++ src/components/ColumnHorseList.vue
@@ -0,0 +1,50 @@
+<template>
+  <BaseColumn title="Horse list" header-bg-color="#f2eb67">
+    <TableHorseList :data="data" />
+  </BaseColumn>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import BaseColumn from "@/components/base/BaseColumn.vue";
+import TableHorseList from "@/components/common/TableHorseList.vue";
+
+const data = ref([
+  {
+    id: 1,
+    name: "horse 1",
+    condition: 59,
+    color: "white",
+  },
+  {
+    id: 2,
+    name: "horse 2",
+    condition: 23,
+    color: "black",
+  },
+  {
+    id: 3,
+    name: "horse 3",
+    condition: 23,
+    color: "red",
+  },
+  {
+    id: 11,
+    name: "horse 1",
+    condition: 59,
+    color: "white",
+  },
+  {
+    id: 12,
+    name: "horse 2",
+    condition: 23,
+    color: "black",
+  },
+  {
+    id: 13,
+    name: "horse 3",
+    condition: 23,
+    color: "red",
+  },
+]);
+</script>
