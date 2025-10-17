671c4092d2154b53adf6eb94b49746db510ae691
steve-232 <steve232np@gmail.com>
2025-10-17 07:49:30 +0200

    add TableHorseList.vue

---
+++ src/components/common/TableHorseList.vue
@@ -0,0 +1,34 @@
+<template>
+  <Table>
+    <TableRow>
+      <TableHeader>Name</TableHeader>
+      <TableHeader>Condition</TableHeader>
+      <TableHeader>Color</TableHeader>
+    </TableRow>
+    <TableRow v-for="horse in data" :key="horse.id">
+      <TableCell>{{ horse.name }}</TableCell>
+      <TableCell>{{ horse.condition }}</TableCell>
+      <TableCell>{{ horse.color }}</TableCell>
+    </TableRow>
+  </Table>
+</template>
+
+<script setup lang="ts">
+import Table from "@/components/base/BaseTable.vue";
+import TableRow from "@/components/base/BaseTableRow.vue";
+import TableHeader from "@/components/base/BaseTableHeader.vue";
+import TableCell from "@/components/base/BaseTableCell.vue";
+
+interface Horse {
+  id: number;
+  name: string;
+  condition: number;
+  color: string;
+}
+
+interface Props {
+  data: Horse[];
+}
+
+defineProps<Props>();
+</script>
