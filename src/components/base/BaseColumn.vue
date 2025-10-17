973fb2e70df6108727e05e86cb0ce5055b67cc60
steve-232 <steve232np@gmail.com>
2025-10-17 07:37:28 +0200

    add BaseColumn.vue

---
+++ src/components/base/BaseColumn.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="column">
+    <header
+      v-if="title.length"
+      class="column__header"
+      :style="[{ textAlign, backgroundColor: headerBgColor }]"
+    >
+      <Title tag="h3">{{ title }}</Title>
+    </header>
+    <div class="column__content">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import Title from "@/components/common/Title.vue";
+
+interface Props {
+  title?: string;
+  textAlign?: "center" | "right" | "left";
+  headerBgColor?: string;
+}
+
+const {
+  title = "",
+  textAlign = "left",
+  headerBgColor = "#fff",
+} = defineProps<Props>();
+</script>
+
+<style scoped lang="scss">
+.column {
+  &__header {
+    padding: 15px 20px;
+    background-color: var(--color-bg-secondary);
+    border: 1px solid var(--color-border-primary);
+  }
+  &__content {
+    overflow-y: scroll;
+  }
+}
+</style>
