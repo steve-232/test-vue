a8ec30abae40c983a21cd4c3c8bbdd8ad7da6a33
steve-232 <steve232np@gmail.com>
2025-10-22 20:03:04 +0200

    add Text.vue

---
+++ src/components/common/Text.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { h } from "vue";
+
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: "p",
+      validator(value: string) {
+        return ["p", "span", "i", "b"].includes(value);
+      },
+    },
+  },
+  setup({ tag }, { slots }) {
+    return () => h(tag, slots.default ? slots.default() : "");
+  },
+};
+</script>
