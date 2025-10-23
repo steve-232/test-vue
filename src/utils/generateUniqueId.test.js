82221056a7761abacb47dfe1dc1ff8b7003252bd
steve-232 <steve232np@gmail.com>
2025-10-23 08:21:12 +0200

    install vitest and cover utils functions with tests

---
+++ src/utils/generateUniqueId.test.js
@@ -0,0 +1,18 @@
+import { expect, it, describe } from "vitest";
+import { generateUniqueId } from "./generateUniqueId";
+
+describe("generateUniqueId", () => {
+  it("should return a number", () => {
+    const id = generateUniqueId();
+    expect(typeof id).toBe("number");
+    expect(Number.isFinite(id)).toBe(true);
+  });
+
+  it("should return different values on multiple calls", () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(generateUniqueId());
+    }
+    expect(ids.size).toBe(50);
+  });
+});
